13acf6d3b77f46686b228e04917575cb67fc306d
Stargazer-ger <125033581+Stargazer-ger@users.noreply.github.com>
2023-05-27 15:52:39 +0200

    Create master.py
    Create Master.py for data pipeline

---
+++ pipeline/master.py
@@ -0,0 +1,11 @@
+import matplotlib as plt
+import numpy as np
+import pandas
+from PIL import Image
+import cv2
+import os
+
+
+def pic_import()
+
+with Image.open("Stargazer-ger/pic_editing/example_pics/landwehr.jpg")
